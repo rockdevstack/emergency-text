cd663b8e77fa0f5a6ea5103e969ff66f509d1312
Wesley Carlsen <wcarlsen@linfirmware.com>
2016-04-06 14:32:30 -0400

    Initial commit

---
+++ app/src/main/java/com/linfirmware/emergencytext/CommonEmergencies.java
@@ -0,0 +1,13 @@
+package com.linfirmware.emergencytext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommonEmergencies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_emergencies);
+    }
+}
