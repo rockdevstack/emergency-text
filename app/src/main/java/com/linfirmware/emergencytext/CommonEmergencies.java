78c895861a293dbc35ca0c53e453bfa64498b5db
Wesley Carlsen <wcarlsen@linfirmware.com>
2016-04-06 15:15:48 -0400

    Send text message via button press (tutorial 1)

--- app/src/main/java/com/linfirmware/emergencytext/CommonEmergencies.java
+++ app/src/main/java/com/linfirmware/emergencytext/CommonEmergencies.java
@@ -2,6 +2,9 @@ package com.linfirmware.emergencytext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
 public class CommonEmergencies extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class CommonEmergencies extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_emergencies);
+    }
+
+    public void sendText(View view)
+    {
+        //Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("6505551212", null, "Test Text Message", null, null);
     }
 }
